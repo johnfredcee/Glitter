b5c5e88da37602a5c6e299fae029440df8d03334
John Connors <johnfredcee@gmail.com>
2017-12-16 22:41:58 +0000

    Working on port of fbx import from Lumix engine.

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -8,9 +8,9 @@
 // Standard Headers
 #include <cstdio>
 #include <cstdlib>
-//#include <map>
-//#include <memory>
-//#include <vector>
+#include <map>
+#include <memory>
+#include <vector>
 //#include <assimp/importer.hpp>
 //#include <assimp/postprocess.h>
 //#include <assimp/scene.h>
@@ -18,6 +18,262 @@
 #include "renderable.h"
 
 ofbx::IScene* g_scene = nullptr;
+
+struct FBXImporter
+{
+	enum class Orientation
+	{
+		Y_UP,
+		Z_UP,
+		Z_MINUS_UP,
+		X_MINUS_UP,
+		X_UP
+	};
+
+	struct RotationKey
+	{
+		glm::quat rot;
+		float time;
+		uint16_t frame;
+	};
+
+	struct TranslationKey
+	{
+		glm::vec3 pos;
+		float time;
+		uint16_t frame;
+	};
+
+	struct Skin
+	{
+		float weights[4];
+		int16_t joints[4];
+		int count = 0;
+	};
+
+	struct ImportAnimation
+	{
+		struct Split
+		{
+			int from_frame = 0;
+			int to_frame = 0;
+			std::string name;
+		};
+
+		const ofbx::AnimationStack* fbx = nullptr;
+		const ofbx::IScene* scene = nullptr;
+		std::vector<Split> splits;
+	    std::string output_filename;
+		bool import = true;
+		int root_motion_bone_idx = -1;
+	};
+
+	struct ImportTexture
+	{
+		enum Type
+		{
+			DIFFUSE,
+			NORMAL,
+			COUNT
+		};
+
+		const ofbx::Texture* fbx = nullptr;
+		bool import = true;
+		bool to_dds = true;
+		bool is_valid = false;
+		std::string path;
+		std::string src;
+	};
+
+	struct ImportMaterial
+	{
+		const ofbx::Material* fbx = nullptr;
+		bool import = true;
+		bool alpha_cutout = false;
+		ImportTexture textures[ImportTexture::COUNT];
+		char shader[20];
+	};
+
+	struct ImportMesh
+	{
+		ImportMesh()
+		{
+		}
+
+		const ofbx::Mesh* fbx = nullptr;
+		const ofbx::Material* fbx_mat = nullptr;
+		bool import = true;
+		bool import_physics = false;
+		int lod = 0;
+		//OutputBlob vertex_data;
+		std::vector<int> indices;
+		float radius_squared;
+	};
+
+    const ofbx::Mesh* getAnyMeshFromBone(const ofbx::Object* node) const
+	{
+		for (int i = 0; i < meshes.size(); ++i)
+		{
+			const ofbx::Mesh* mesh = meshes[i].fbx;
+
+			auto* skin = mesh->getGeometry()->getSkin();
+			if (!skin) continue;
+
+			for (int j = 0, c = skin->getClusterCount(); j < c; ++j)
+			{
+				if (skin->getCluster(j)->getLink() == node) return mesh;
+			}
+		}
+		return nullptr;
+	}
+
+    static ofbx::Matrix makeOFBXIdentity() { return {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1}; }
+
+
+	static ofbx::Matrix getBindPoseMatrix(const ofbx::Mesh* mesh, const ofbx::Object* node)
+	{
+		if (!mesh) return makeOFBXIdentity();
+
+		auto* skin = mesh->getGeometry()->getSkin();
+
+		for (int i = 0, c = skin->getClusterCount(); i < c; ++i)
+		{
+			const ofbx::Cluster* cluster = skin->getCluster(i);
+			if (cluster->getLink() == node)
+			{
+				return cluster->getTransformLinkMatrix();
+			}
+		}
+		assert(false);
+		return makeOFBXIdentity();
+	}
+
+	static glm::vec3 getTranslation(const ofbx::Matrix& mtx)
+	{
+		return glm::vec3((float)mtx.m[12], (float)mtx.m[13], (float)mtx.m[14]);
+	}
+
+
+	static glm::quat getRotation(const ofbx::Matrix& mtx)
+	{
+		glm::mat4x4 m = glm::make_mat4x4(mtx.m);
+		glm::quat q = glm::quat_cast(m);
+		return q;
+	}
+
+	static float getScaleX(const ofbx::Matrix& mtx)
+	{
+		glm::vec3 v(float(mtx.m[0]), float(mtx.m[4]), float(mtx.m[8]));
+
+		return v.length();
+	}
+
+	static int getDepth(const ofbx::Object* bone)
+	{
+		int depth = 0;
+		while (bone)
+		{
+			++depth;
+			bone = bone->getParent();
+		}
+		return depth;
+	}
+
+	bool isSkinned(const ofbx::Mesh& mesh) const { return !ignore_skeleton && mesh.getGeometry()->getSkin() != nullptr; }
+
+    glm::vec3 fixRootOrientation(const glm::vec3& v) const
+	{
+		switch (root_orientation)
+		{
+			case Orientation::Y_UP: return glm::vec3(v.x, v.y, v.z);
+			case Orientation::Z_UP: return glm::vec3(v.x, v.z, -v.y);
+			case Orientation::Z_MINUS_UP: return glm::vec3(v.x, -v.z, v.y);
+			case Orientation::X_MINUS_UP: return glm::vec3(v.y, -v.x, v.z);
+			case Orientation::X_UP: return glm::vec3(-v.y, v.x, v.z);
+		}
+		assert(false);
+		return glm::vec3(v.x, v.y, v.z);
+	}
+
+
+	glm::quat fixRootOrientation(const glm::quat& v) const
+	{
+		switch (root_orientation)
+		{
+			case Orientation::Y_UP: return glm::quat(v.x, v.y, v.z, v.w);
+			case Orientation::Z_UP: return glm::quat(v.x, v.z, -v.y, v.w);
+			case Orientation::Z_MINUS_UP: return glm::quat(v.x, -v.z, v.y, v.w);
+			case Orientation::X_MINUS_UP: return glm::quat(v.y, -v.x, v.z, v.w);
+			case Orientation::X_UP: return glm::quat(-v.y, v.x, v.z, v.w);
+		}
+		assert(false);
+		return glm::quat(v.x, v.y, v.z, v.w);
+	}
+
+
+	glm::vec3 fixOrientation(const glm::vec3& v) const
+	{
+		switch (orientation)
+		{
+			case Orientation::Y_UP: return glm::vec3(v.x, v.y, v.z);
+			case Orientation::Z_UP: return glm::vec3(v.x, v.z, -v.y);
+			case Orientation::Z_MINUS_UP: return glm::vec3(v.x, -v.z, v.y);
+			case Orientation::X_MINUS_UP: return glm::vec3(v.y, -v.x, v.z);
+			case Orientation::X_UP: return glm::vec3(-v.y, v.x, v.z);
+		}
+		assert(false);
+		return glm::vec3(v.x, v.y, v.z);
+	}
+
+
+	glm::quat fixOrientation(const glm::quat& v) const
+	{
+		switch (orientation)
+		{
+			case Orientation::Y_UP: return glm::quat(v.x, v.y, v.z, v.w);
+			case Orientation::Z_UP: return glm::quat(v.x, v.z, -v.y, v.w);
+			case Orientation::Z_MINUS_UP: return glm::quat(v.x, -v.z, v.y, v.w);
+			case Orientation::X_MINUS_UP: return glm::quat(v.y, -v.x, v.z, v.w);
+			case Orientation::X_UP: return glm::quat(-v.y, v.x, v.z, v.w);
+		}
+		assert(false);
+		return glm::quat(v.x, v.y, v.z, v.w);
+	}
+
+
+	void clearSources()
+	{
+		for (ofbx::IScene* scene : scenes) scene->destroy();
+		scenes.clear();
+		meshes.clear();
+		materials.clear();
+		animations.clear();
+		bones.clear();
+	}
+
+	std::vector<ImportMaterial> materials;
+	std::vector<ImportMesh> meshes;
+	std::vector<ImportAnimation> animations;
+	std::vector<const ofbx::Object*> bones;
+	std::vector<ofbx::IScene*> scenes;
+	float lods_distances[4] = {-10, -100, -1000, -10000};
+    float mesh_scale = 1.0f;
+	float time_scale = 1.0f;
+	float position_error = 0.1f;
+	float rotation_error = 0.01f;
+	float bounding_shape_scale = 1.0f;
+	bool to_dds = false;
+	bool center_mesh = false;
+	bool ignore_skeleton = false;
+	bool import_vertex_colors = true;
+	bool make_convex = false;
+	bool create_billboard_lod = false;
+	Orientation orientation = Orientation::Y_UP;
+	Orientation root_orientation = Orientation::Y_UP;
+	
+
+};
+
 
 int main(int argc, char * argv[]) {
 
